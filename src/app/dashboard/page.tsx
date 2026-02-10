dd58c32e0eba39124219e5f325118fb806f56387
RohitMaurya139 <rohit139maurya@gmail.com>
2026-02-10 20:20:23 +0530

    HelpDesk AI push

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
+
+import DashboardClient from "@/components/DashboardClient";
+import { getSession } from "@/lib/getSession";
+import React from "react";
+
+async function page() {
+  const session = await getSession();
+
+  return (
+    <>
+      <DashboardClient ownerId={session?.user?.id!} />
+    </>
+  );
+}
+
+export default page;
